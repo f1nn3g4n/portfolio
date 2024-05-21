d0be6a1c36e1c938f393cc6532d74490378e2c5d
Finn Tobin <finngoodfellow@gmail.com>
2024-05-21 14:45:17 -0700

    fix note and start display

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,13 @@ import { lobby } from './scenes/lobby/lobby';
 k.setBackground(k.Color.BLACK);
 
 const note = document.getElementById("note");
+const start = document.getElementById("start");
 
 k.scene("start", () => {
-  k.add([
-    k.text("Welcome! Click anywhere to start", { size: 24 }),
-    k.Color.WHITE,
-    k.anchor('center'),
-    k.pos(0)
-  ]);
-
-  k.camPos(0, 0);
-
   k.onMousePress(() => {
     k.go("lobby");
-    note!.style.display = "flex";
+    start!.style.display = "none";
+    note!.style.display = "table";
   })
 });
 
